bca1feba960b9389603f8e4e7190748183ea7ec0
agnmeinardo <agnmeinardo@outlook.com>
2021-01-03 12:09:39 -0300

    GET y POST de /categoria

--- app.js
+++ app.js
@@ -32,6 +32,68 @@ conexion.connect((error) => {
 // Permito el uso de async-await en la conexión para que no se quede trabaja la aplicación esperando la respuesta. Con async-await se hace asíncronamente la respuesta (sigo ejecutando lo demás de la aplicación).
 const qy = util.promisify(conexion.query).bind(conexion);
 
-// Comentario
+/*
+*   Definición de los métodos HTTP
+*
+*/
+
+// Categoría
+
+app.get('/categoria', async (req, res) => {
+
+    try{
+
+        const query = "SELECT * FROM categoria";
+        const respuesta = await qy(query);
+
+        res.status(200).send(respuesta);
+
+    }
+    catch(e){
+        console.log(e.message);
+        res.status(413).send([]);
+    }
+
+});
+
+app.post('/categoria', async (req, res) => {
+
+    try{
+
+        var nombre = req.body.nombre.toUpperCase();
+
+        // Realizo validaciones
+        if(!nombre){
+            throw new Error('Faltan datos');
+        }
+
+        let query = "SELECT * FROM categoria WHERE nombre = ? ";
+        let respuesta = await qy(query,[nombre]);
+
+        if(respuesta.length > 0){
+            throw new Error('Ese nombre de categoria ya existe');
+        }
+
+        query = "INSERT INTO categoria (nombre) VALUE (?)";
+        respuesta = await qy(query,[nombre]);
+
+        console.log(respuesta);
+        res.status(200).send({"id": respuesta.insertId, "nombre": nombre});
+
+    }
+    catch(e){
+        console.log(e.message);
+        res.status(413).send({ "message": e.message});
+    }
+
+});
+
+// Hasta acá anda bien
+
+
+
+
+
+
 
 app.listen(port, () => { console.log("Servidor en escucha en el puerto " + port)});
